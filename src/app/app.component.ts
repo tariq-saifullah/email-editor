c0cc1b8f2fb19589d0bc6e4278718e8a6ff0929c
tariq-saifullah <tariq.saifullah@spursol.com>
2022-12-27 00:34:32 +0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { EmailEditorComponent } from 'angular-email-editor';
+import { MatDialog } from "@angular/material/dialog";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+
+  @ViewChild('editor') private emailEditor: EmailEditorComponent | undefined;
+  @ViewChild('imageGalleryModal') imageGalleryModal: any;
+
+  selectedImage: string = '';
+
+
+  constructor(public dialog: MatDialog) { }
+
+  ngOnInit() { }
+
+  editorLoaded(event: any) {
+    this.emailEditor?.editor.addEventListener('selectImage', (data: any, done: any) => {
+
+      let dialogRef = this.dialog.open(this.imageGalleryModal, {
+      });
+
+      dialogRef.afterClosed().subscribe(result => {
+        done({ url: this.selectedImage });
+      });
+    });
+  }
+
+  onSelectImage(url: string) {
+    this.selectedImage = '';
+    this.selectedImage = url;
+    this.dialog.closeAll();
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { EmailEditorModule } from 'angular-email-editor';
+
+// material imports here...
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatDialogModule} from '@angular/material/dialog';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    EmailEditorModule,
+
+    MatSlideToggleModule,
+    MatDialogModule
+  ],
+  exports: [
+    MatDialogModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
